7814edc6322ec3693783ec160b1b3335f376fd0d
buise <1078688845@qq.com>
2017-05-07 23:50:46 +0800

    fh

--- js/zxq-dibu.js
+++ js/zxq-dibu.js
@@ -1,20 +1,27 @@
 $(document).ready(function(){
-	var audio=$('audio').get(0);
-	$('.zxq-play2').on('click',function(){
+
+    $('.zxq-bottom')[0].addEventListener('touchstart', function(e) {
+        var obj=e.target;
+        console.log(obj);
+        if($(obj).attr('src')=='../img/zxq-bf.png'){
+            var audio=$('audio').get(0);
 			if(audio.paused){
 				audio.play();
 			}else{
 				audio.pause();
 			}
-	})
-	var jindu=$('.zxq-bottom .zxq-tiao');
-	var tiao=$('.zxq-bottom .zxq-ctiao');
-	$('.zxq-bottom .zxq-yuan').on('click',false);
-	jindu.on('click',function(e){
-		audio.currentTime=audio.duration*(e.offsetX/jindu.width());
-	})
-	$(audio).on('timeupdate',function(){
-			var w=jindu.width()*audio.currentTime/audio.duration;
-			tiao.width(w);
-	});
+            var jindu=$('.zxq-bottom .zxq-tiao');
+            var tiao=$('.zxq-bottom .zxq-ctiao');
+            $('.zxq-bottom .zxq-yuan').on('click',false);
+            jindu.on('click',function(e){
+                audio.currentTime=audio.duration*(e.offsetX/jindu.width());
+            })
+            $(audio).on('timeupdate',function(){
+                var w=jindu.width()*audio.currentTime/audio.duration;
+                tiao.width(w);
+            });
+		}else{
+        	location.href="../html/sc-bofang.html";
+		}
+    })
 })
