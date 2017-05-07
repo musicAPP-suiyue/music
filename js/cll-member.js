ae6b97283927f2edca9076b7c5309490a0726e98
ShenJingBingA <532510674@qq.com>
2017-05-07 17:33:05 +0800

    cll

--- js/cll-member.js
+++
@@ -1,8 +0,0 @@
-/**
- * Created by Administrator on 2017/5/3.
- */
-$(document).ready(function (){
-    $('.dredge').on('touchstart',function () {
-        location.href='https://htmlpreview.github.io/?https://github.com/musicAPP-suiyue/music/blob/master/html/cll-member-click.html';
-    })
-})
