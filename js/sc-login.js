67fda31015ef3740a2a156f2c1f84f5ce1ac0030
buise <1078688845@qq.com>
2017-05-08 08:49:25 +0800

    fh

--- js/sc-login.js
+++ js/sc-login.js
@@ -3,14 +3,12 @@
  */
 $(document).ready(function () {
     $('.login').on('touchstart',function () {
-        let name=$('.from-1 > input').val();
-        let pass=$('.from-2 > input').val();
+        var name=$('.from-1 > input').val();
+        var pass=$('.from-2 > input').val();
         if(name=="1078688845"&&pass=="lol7758521"){
-            location.href='../html/index1.html'
+            location.href='../html/index.html'
         }else{
             $('.cuowu-1').css('display','flex');
         }
     })
-
-
 })
